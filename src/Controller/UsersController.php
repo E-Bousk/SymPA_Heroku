22e7081cb738b43d48ecfc3d01081e2b7f313d1b
E.Bousk <e.bousk@gmail.com>
2021-12-17 17:07:01 +0100

    Automatic slugs and dates with 'Gedmo'

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Annonces;
+use App\Form\AnnoncesType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/users")
+ * @package App\Controller
+ */
+class UsersController extends AbstractController
+{
+    /**
+     * @Route("/", name="users")
+     */
+    public function index(): Response
+    {
+        return $this->render('users/index.html.twig', [
+            'controller_name' => 'UsersController',
+        ]);
+    }
+
+    /**
+     * @Route("/annonces/ajout", name="users_annonces_ajout")
+     */
+    public function ajoutAnnonce(Request $request): Response
+    {
+        $annonce = new Annonces;
+
+        $form = $this->createForm(AnnoncesType::class, $annonce);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $annonce->setActive(false);
+            $annonce->setUsers($this->getUser());
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($annonce);
+            $em->flush();
+
+            return $this->redirectToRoute('users');
+        }
+
+        return $this->render('users/annonces/ajout.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
